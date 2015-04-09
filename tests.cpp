2bc79611a40a6bc0268825e3f158c846e4d3f353
nick <nrclark@essex.ac.uk>
2015-04-09 03:09:51 +0100

    made a more aggressive test with chrono and thread

--- tests.cpp
+++ tests.cpp
@@ -1,7 +1,8 @@
 #include "google/gtest/include/gtest/gtest.h"
 #include "google/gmock/include/gmock/gmock.h"
 
-
+#include <chrono>
+#include <thread>
 
 float sum(float a, float b) 
 {
@@ -19,6 +20,20 @@ TEST(Sum, Normal) {
 	auto b = 3.f;
 	
   	EXPECT_THAT(   sum(a, b),  FloatEq(a+b)   );
+}
+
+
+TEST(Libstdcxx, Trying_to_break_it) {
+	//Using a matcher here from gmock
+	
+	auto duration  = std::chrono::seconds(1);
+	
+	auto start = std::chrono::high_resolution_clock::now();
+    std::this_thread::sleep_for( duration );
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::nano> elapsed = end-start;
+	
+  	EXPECT_GT(   elapsed,  std::chrono::nanoseconds(duration)   );
 }
 
 
